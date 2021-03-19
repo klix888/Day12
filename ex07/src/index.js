61112bd0a2be01c63895fd889657ce14dd6239f5
Dzevad <ynwa886@gmail.com>
2021-03-19 22:51:49 +0100

    My Music Collection

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,20 +1,26 @@
 var collection = {
     2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+      albumTitle: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: [
+          "Let It Rock", 
+          "You Give Love a Bad Name"
+        ]
     },
     2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
+      albumTitle: "1999",
+      artist: "Prince",
+      tracks: [
+        "1999", 
+        "Little Red Corvette"
+    ]
     },
     1245: {
-      artist: 'Robert Palmer',
-      tracks: []
+      artist: "Robert Palmer",
+      tracks: [ ]
     },
     5439: {
-      albumTitle: 'ABBA Gold'
+      albumTitle: "ABBA Gold"
     }
   };
 
